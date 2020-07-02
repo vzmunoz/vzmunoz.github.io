c13fcdf6c4254ced243f1b47e75d2a56af0ddd60
Valentina <vzmunoz@uc.cl>
2020-07-01 20:07:21 -0400

    probando

---
+++ java.js
@@ -0,0 +1,98 @@
+//http://127.0.0.1:8887/
+let posicionActual = 0;
+
+const TEXTOS = [
+    'text1.json',
+    'text2.json',
+    'text3.json',
+    'text4.json',
+    'text5.json'
+];
+const IMAGENES = [
+    'img/1.png',
+    'img/2.png',
+    'img/3.png',
+    'img/4.png',
+    'img/5.png'
+];
+
+window.onload = function () {
+// Variable
+
+const TIEMPO_INTERVALO_MILESIMAS_SEG = 1000;
+let $botonRetroceder = document.querySelector('#retroceder');
+let $botonAvanzar = document.querySelector('#avanzar');
+let $imagen = document.querySelector('#imagen');
+let $botonActualice =document.querySelector('#btn');
+let intervalo;
+
+// Funciones
+
+
+function pasarFoto() {
+    if(posicionActual >= IMAGENES.length - 1) {
+        posicionActual = 0;
+    } else {
+        posicionActual++;
+    }
+    renderizarImagen();
+}
+
+/**
+ * Funcion que cambia la foto en la anterior posicion
+ */
+function retrocederFoto() {
+    if(posicionActual <= 0) {
+        posicionActual = IMAGENES.length - 1;
+    } else {
+        posicionActual--;
+    }
+    renderizarImagen();
+}
+/**
+ * Funcion que cambia la foto en la siguiente posicion
+ */
+
+/**
+ * Funcion que actualiza la imagen de imagen dependiendo de posicionActual
+ */
+function renderizarImagen () {
+    $imagen.style.backgroundImage = `url(${IMAGENES[posicionActual]})`;
+}
+/**
+ * Para el autoplay de la imagen
+ */
+
+function traerDatos(){
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET',TEXTOS[posicionActual],true);
+    xhttp.send();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            let datos = JSON.parse(this.responseText);
+            console.log(datos.length);
+            if(datos.length == 0){
+                alert('Revisa la camara raspberry, no se estan detectando datos de pacientes');
+            }else{
+                let res =document.querySelector('#actualice');
+                res.innerHTML='';
+                for(let item of datos){
+                    //console.log(item);
+                    res.innerHTML += "<tr><td>"+ item.Names + "</td><td>" + item.val + "</td><td>";
+                }
+            }
+        }
+    }
+}
+// Eventos
+$botonAvanzar.addEventListener('click', pasarFoto);
+$botonRetroceder.addEventListener('click', retrocederFoto);
+$botonActualice.addEventListener('click',traerDatos);
+// Iniciar
+renderizarImagen();
+
+    
+return posicionActual
+
+}
+
